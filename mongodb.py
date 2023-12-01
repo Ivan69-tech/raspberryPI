7e20f3a0b0125745ceaacb4970f04ae8108321cc
Ivan <ivan.pouradierduteil@gmail.com>
2023-12-01 19:34:13 +0100

    new feature

--- mongodb.py
+++ mongodb.py
@@ -1,11 +1,21 @@
 from pymongo import MongoClient
 
+import time 
+
 
 cluster = MongoClient("mongodb+srv://Ivan:malivajo@cluster0.tue7chl.mongodb.net/?retryWrites=true&w=majority")
 
-db = cluster["test"]
-collection = db["test"]
+db = cluster["test2"]
+collection = db["test2"]
+collection.delete_many({})
+k = 0
 
-post = {"_id" :1, "name": "ok"}
 
-collection.insert_one(post)
+
+while True :
+  k += 1
+  post = {"_id" :k, "name": f"{k}"}
+  collection.insert_one(post)
+  print(f"id {k} posté")
+  time.sleep(5)
+
